e2d714099d66091786a7fcb431bb58f63eadea7a
Nariman <narimanid@gmail.com>
2023-07-28 01:09:46 +0600

    Trashbox

---
+++ src/test/java/selenium/magentotests/TestThash.java
@@ -0,0 +1,17 @@
+package selenium.magentotests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import selenium.BaseTest;
+import selenium.ui.helpers.ElementActions;
+
+public class TestThash extends BaseTest {
+    @Test
+    void test23(){
+        driver.get("https://magento.softwaretestingboard.com/gear/bags.html");
+       WebElement trashbox = driver.findElement(By.xpath("//a[@class='action showcart']"));
+        ElementActions elementActions = new ElementActions();
+        elementActions.clickTheButton(trashbox);
+    }
+}
